5c28006459f5ebde895e64a25b789487ec0ad7a2
vivak <vivak.k@philips.com>
2019-01-27 16:49:33 +0530

    adding of person tag

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
@@ -7,6 +8,7 @@ class App extends Component {
       <div className="App">
         <h1>I am a react project</h1>
         <p>This is working really!!!</p>
+        <Person/>
       </div>
     );
     // return React.createElement('div',{className: "App"} ,React.createElement('h1',null ,'Hi i am vivak'));
---
+++ src/Person/Person.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Person = () => {
+    
+    return(<div>
+        <p>This is awsome</p>
+    </div>);   
+
+}
+
+export default Person;
