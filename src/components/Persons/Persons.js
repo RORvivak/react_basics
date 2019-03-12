2796744972327016f55a6fb1d65973a8b123bddf
vivak <vivak.k@philips.com>
2019-03-12 22:21:45 +0530

    adding of pure component

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,17 +1,21 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import Person from './Person/Person';
 
-class Persons extends Component
+class Persons extends PureComponent
 { 
   static getDerivedStateFromProps(prevprops, prevstate)
   {
-    console.log("[persons.js] getDerivedStateFromProps")
+    console.log("[persons.js] getDerivedStateFromProps"                   )
   }
   
-  shouldComponentUpdate(nextprops, nextstate){
-    return true
-  }
-
+    // shouldComponentUpdate(nextprops, nextstate){
+    //   if(this.props.persons !== nextprops.persons){
+    //   return true
+    // }
+    // else{
+    //   return false
+    // }
+    // }
   getSnapshotBeforeUpdate(prevprops, prevstate){
     console.log("[persons.js] getSnapshotBeforeUpdate")
     return{ message: "vivak"}
