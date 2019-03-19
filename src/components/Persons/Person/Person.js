c01e94f98cb128904521c9aa0a9d5aa1ca44413b
vivak <vivak.k@philips.com>
2019-03-19 21:04:21 +0530

    ref

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,6 +4,9 @@ import './Person.css';
 import Radium from 'radium';
 
 class Person extends Component {
+  componentDidMount(){
+    this.ref.focus();
+  }
   render(){ 
   const style = {
         '@media (min-width: 500px)':{
@@ -14,7 +17,7 @@ class Person extends Component {
     return(<div className = 'Person' style={style} >
         <p onClick = {this.props.remove} >I am {this.props.name} and my age is {this.props.age}</p>
         <p>{this.props.children}</p>
-        <input type="text"  onChange = {this.props.change}/>
+        <input type="text"  onChange = {this.props.change} ref={ (ref_id)=> this.ref = ref_id}/>
         <p>Password Check</p> 
         <input type="text" onChange = {this.props.length} value = {this.props.value}/>
     </div>);   
